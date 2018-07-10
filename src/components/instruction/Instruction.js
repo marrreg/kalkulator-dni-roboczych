517e253d740a042848829fa374d082b94d58c862
Marek Koszlak <koszlak.marek@gmail.com>
2018-07-10 22:59:07 +0200

    Added instruction on the bottom of the Paper element. It's meant to display the current instruction for user, based on existing application state

---
+++ src/components/instruction/Instruction.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Instruction extends React.Component {
+  render() {
+    return (
+      <p>Wypełnij dwa z powyższych pól</p>
+        // <img
+        //     className="logo-img"
+        //     alt="Logo"
+        //     src="https://arizonachristian.edu/wp-content/uploads/2017/06/logo-placeholder.png"
+        // />
+    );
+  }
+}
+
+Instruction.propTypes = {};
+
+export default Instruction;
